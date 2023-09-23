2394cf7e4feb5dd7b884339dd954e1ae56106996
JangSeongYeong <syzxz0608@gmail.com>
2023-09-24 00:54:37 +0900

    2개의 신호를 받았을 경우 조치 추가

--- js/dataSend.js
+++ js/dataSend.js
@@ -1,15 +1,12 @@
 const clearBtn = document.querySelector("#clearBtn");
 const modal = document.querySelector("#myModal");
 const reportBtn = document.querySelector("#reportBtn");
+const reportBtn1 = document.querySelector("#reportBtn1");
+const reportBtn2 = document.querySelector("#reportBtn2");
+const modalContent = document.querySelector("#myModal .modal-content p");
 
-/*
-socketData에 urlParams, ipAddress가 선언되어있어 선언할 필요 없음
-같은 html안에 자바스크립트를 넣는 것이기 때문에 
-<script>안에 둘다 들어간다고 생각하면 같은 객체가 겹치거나 같은 변수를 사용해서는 안됨
-*/
 function openModal(cameraNumber) {
     modal.classList.remove(HIDDEN_CLASSNAME);
-    const modalContent = document.querySelector("#myModal .modal-content p");
     modalContent.innerText = `${cameraNumber}번 카메라 위치에서 신호가 들어왔습니다.`;
 }
 
@@ -20,11 +17,13 @@ function closeModal() {
     sData = "0";
 }
 
+
 const ws8767 = new WebSocket(`ws://${ipAddress}:8767`);
 
 clearBtn.addEventListener("click", () => {
     const message = "clear";
     closeModal();
+    CRASHCONTROL = false;
     ws8767.send(message);
 });
 
@@ -32,6 +31,24 @@ reportBtn.addEventListener("click", () => {
     if(confirm("신고하시겠습니까?")){
         alert(`신고되었습니다.`)
         closeModal();
+    }
+})
+
+reportBtn1.addEventListener("click", () => {
+    if(confirm("1번 위치로 신고하시겠습니까?")){
+        alert(`신고되었습니다.`)
+        modal.classList.add(HIDDEN_CLASSNAME);
+        sData = "0";
+        CRASHCONTROL = false;
+    }
+})
+
+reportBtn2.addEventListener("click", () => {
+    if(confirm("2번 위치로 신고하시겠습니까?")){
+        alert(`신고되었습니다.`)
+        modal.classList.add(HIDDEN_CLASSNAME);
+        sData = "0";
+        CRASHCONTROL = false;
     }
 })
 
--- js/drCamSizeAdjustment.js
+++ js/drCamSizeAdjustment.js
@@ -1,4 +1,7 @@
+const crashBtn = document.querySelector("#crashBtn"); //다른 신호 들어왔을때
 const HIDDEN_CLASSNAME = "hidden"; // 감추는 class
+let sData = "0";
+let CRASHCONTROL = false;
 
 function setWebcamSize(cam, size) { // 화면 확대
     cam.style.width = size;
@@ -11,6 +14,16 @@ function resetWebcam() { // 초기 화면으로
     cam2.classList.remove(HIDDEN_CLASSNAME);
 }
 
+crashBtn.addEventListener("click", () => { // 다른 신호로 인해 생긴 버튼 클릭시
+    CRASHCONTROL = true;
+    crashBtn.classList.add(HIDDEN_CLASSNAME);
+    reportBtn.classList.add(HIDDEN_CLASSNAME);
+    modalContent.innerText = `확인 후 적절한 조치를 부탁드립니다.`;
+    resetWebcam();
+    reportBtn1.classList.remove(HIDDEN_CLASSNAME);
+    reportBtn2.classList.remove(HIDDEN_CLASSNAME);
+});
+
 const ws8766 = new WebSocket(`ws://${ipAddress}:8766`);
 
 ws8766.onopen = function () {
@@ -20,18 +33,33 @@ ws8766.onopen = function () {
 
 ws8766.onmessage = (event) => {
     const Data = event.data
-    if (Data === "1") { 
-        setWebcamSize(cam1, "100%");
-        cam2.classList.add(HIDDEN_CLASSNAME);
-        cam1.classList.remove(HIDDEN_CLASSNAME);
-        openModal(Data);
-        console.log("cam1"); //이 줄 기준 위에 5줄 함수화 시키기
-    } else if (Data === "2") {
-        setWebcamSize(cam2, "100%");
-        cam1.classList.add(HIDDEN_CLASSNAME);
-        cam2.classList.remove(HIDDEN_CLASSNAME);
-        openModal(Data);
-        console.log("cam2");
+    if(CRASHCONTROL === false){
+        reportBtn1.classList.add(HIDDEN_CLASSNAME);
+        reportBtn2.classList.add(HIDDEN_CLASSNAME);
+        reportBtn.classList.remove(HIDDEN_CLASSNAME);
+        if (Data === "1") { 
+            if (sData !== Data && sData !== "0"){
+                crashBtn.classList.remove(HIDDEN_CLASSNAME);
+            } else {   
+                sData = Data;
+                setWebcamSize(cam1, "100%");
+                cam2.classList.add(HIDDEN_CLASSNAME);
+                cam1.classList.remove(HIDDEN_CLASSNAME);
+                openModal(Data);
+                console.log("cam1"); //이 줄 기준 위에 5줄 함수화 시키기
+            }
+        } else if (Data === "2") {
+            if (sData !== Data && sData !== "0"){
+                crashBtn.classList.remove(HIDDEN_CLASSNAME);
+            } else {
+                sData = Data;
+                setWebcamSize(cam2, "100%");
+                cam1.classList.add(HIDDEN_CLASSNAME);
+                cam2.classList.remove(HIDDEN_CLASSNAME);
+                openModal(Data);
+                console.log("cam2");
+            }
+        }
     }
 };
 

1700661049d207c256cea8472344dedf48db0018
JangSeongYeong <syzxz0608@gmail.com>
2023-09-25 01:13:13 +0900

    코드 간략화 및 신고 버튼 조정

--- js/validation.js
+++ js/validation.js
@@ -1,13 +1,14 @@
 const input = document.querySelector("#WebSocketIP");
 const loginForm = document.querySelector("#login-form");
 
+// 유효성 검사 - IP형식으로 입력시 통과
 function validateIPAddress(ipAddress) {
     const ipRegex = /^([0-9]{1,3}\.){3}[0-9]{1,3}$/;
     return ipRegex.test(ipAddress);
 }
 
 loginForm.addEventListener("submit", (event) => {
-    event.preventDefault();  // 기본 submit 동작 방지
+    event.preventDefault(); // 기본 submit 동작 방지
 
     const ipAddress = input.value;
     if (!validateIPAddress(ipAddress)) {

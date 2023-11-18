e56893d8ba705942e7179c875cc9595428e7ee92
JangSeongYeong <syzxz0608@gmail.com>
2023-11-18 14:57:56 +0900

    화면 전환 방식 수정

--- js/camRelevant.js
+++ js/camRelevant.js
@@ -1,47 +1,49 @@
 // ~Btn과 Modal은 modalControl에서 선언
-const cam1 = document.querySelector("#cam1");
-const cam2 = document.querySelector("#cam2");
-const cam1Text = document.querySelector("#cam1Text");
-const cam2Text = document.querySelector("#cam2Text");
+const cam1 = document.querySelector("#cam1");   // 영상 표시용 1
+const cam2 = document.querySelector("#cam2");   // 영상 표시용 2
+const camera1 = document.querySelector(".camera1"); // 영상과 위치 text div 1
+const camera2 = document.querySelector(".camera2"); // 영상과 위치 text div 2
 
-
+// class hidden을 위한 상수 선언
 const HIDDEN_CLASSNAME = "hidden";
-let focusCamValue = "0";
-let crashValue = false;
 
-// IP값 읽어오기
+let focusCamValue = "0";    // 확대 되어있는 카메라 위치 값 (1 or 2)
+let crashValue = false;     // 신호 2개를 받았을 때 생성되는 버튼을 누르면 True - cam확대 조작
+
+// Hader를 통한 IP값 읽어오기
 const urlParams = new URLSearchParams(window.location.search);
 const ipAddress = urlParams.get('ip');
 
-// hidden class 제거
+// 특정 태그의 hidden class 제거 함수
 function removeHidden(element) {
     element.classList.remove(HIDDEN_CLASSNAME);
 }
-// hidden class 추가
+// 특정 태그의 hidden class 추가 함수
 function addHidden(element) {
     element.classList.add(HIDDEN_CLASSNAME);
 }
 
-// cam화면 크기 설정
-function setWebcamSize(cam, size) {
-    cam.style.width = size;
-}
-// cam화면 초기화: 기본 화면
-function resetWebcam() {
-    setWebcamSize(cam1, "50%");
-    setWebcamSize(cam2, "50%");
-    removeHidden(cam1);
-    removeHidden(cam2); 
+// 화면 전환 함수
+function changeCam(outCam, receivedData, reportBtn){
+    focusCamValue = receivedData;
+    addHidden(outCam);          // 신호가 들어온 화면 확대를 위하여 다른 카메라에 hidden 추가
+    openModal(receivedData);    // modal open (receiveData번 카메라 위치에서 신호가 들어왔습니다.)
+    removeHidden(reportBtn);    // 다른 카메라에 해당하는 신고버튼 숨기기
 }
 
-// 수신 값에 따른 화면 전환 코드
-function changeCam(focusCam, outCam, receivedData, reportBtn){
-    focusCamValue = receivedData;
-    setWebcamSize(focusCam, "100%");
-    addHidden(outCam);
-    openModal(receivedData);
-    removeHidden(reportBtn);
+// 화면 초기화 (hidden을 둘다 제거함)
+function resetCam(){
+    if(camera1.classList[1]){
+        removeHidden(camera1);    
+    } else if (camera2.classList[1]){
+        removeHidden(camera2);
+    }
 }
+
+
+// --------------------------------------------------------
+// ---------------- 웹 소켓 관련 코드 -----------------------
+// --------------------------------------------------------
 
 // WebSocket 8765번 연결: 영상 전용 소켓
 const ws8765 = new WebSocket(`ws://${ipAddress}:8765`);
@@ -74,22 +76,22 @@ const ws8766 = new WebSocket(`ws://${ipAddress}:8766`);
 
 ws8766.onopen = function () {
     console.log("WebSocket is connected 8766.");
-    resetWebcam();
 }
 ws8766.onmessage = (event) => {
     const receivedData = event.data;
-    if(crashValue === false){ // 신호가 들어와있는 상태에서 다른 신호가 들어오면 버튼 생성
-        if ((focusCamValue !== receivedData) && (focusCamValue !== "0") && (receivedData !== "0")){
+    // crashValue는 crashBtn(특정 캠이 확대되었을때 다른 신호가 들어오면 발생)을 click시 True로 변경되어 cam확대 방지
+    if(crashValue === false){ 
+        // 신호가 들어와있는 상태에서 다른 신호가 들어오면 버튼 생성
+        if ((focusCamValue !== "0") && (focusCamValue !== receivedData) && (receivedData !== "0")){
             removeHidden(crashBtn);
         }
-        if (receivedData === "1" && focusCamValue !== "2") { // 1번 신호가 들어오면 1번 화면 확대
-            changeCam(cam1,cam2,receivedData,reportBtn1);
-            addHidden(cam2Text);
+        // 1번 신호가 들어오면 1번 화면 확대(다른 카메라가 확대중일시 작동X)
+        if (receivedData === "1" && focusCamValue !== "2") {             
+            changeCam(camera2,receivedData,reportBtn1);
             }
-        else if (receivedData === "2" && focusCamValue !== "1") { // 2번 신호가 들어오면 2번 화면 확대
-            changeCam(cam2,cam1,receivedData,reportBtn2);
-            addHidden(cam1Text);
-            cam2Text.classList.add("change_Location"); // 카메라 2 택스트 위치 조정
+        // 2번 신호가 들어오면 2번 화면 확대(다른 카메라가 확대중일시 작동X)
+        else if (receivedData === "2" && focusCamValue !== "1") {     
+            changeCam(camera1,receivedData,reportBtn2);
         }
         
     }
--- js/modalControl.js
+++ js/modalControl.js
@@ -1,41 +1,36 @@
-const clearBtn = document.querySelector("#clearBtn"); // 해제 버튼
-const mainClearBtn = document.querySelector("#mainClearBtn"); //main페이지 해제 버튼
-const reportBtn1 = document.querySelector("#reportBtn1"); // 신고 1 버튼
-const reportBtn2 = document.querySelector("#reportBtn2"); // 신고 2 버튼
-const crashBtn = document.querySelector("#crashBtn"); // 다중 신호 수신 시 버튼
-const modal = document.querySelector("#myModal"); // modal(알림창)
+const clearBtn = document.querySelector("#clearBtn");           // 해제 버튼
+const mainClearBtn = document.querySelector("#mainClearBtn");   // main페이지 해제 버튼
+const reportBtn1 = document.querySelector("#reportBtn1");       // 신고 1 버튼
+const reportBtn2 = document.querySelector("#reportBtn2");       // 신고 2 버튼
+const crashBtn = document.querySelector("#crashBtn");           // 특정 캠 확대 했을때 다른 신호 수신 시 생성
+const modal = document.querySelector("#myModal");               // modal(알림창)
 const modalContent = document.querySelector("#myModal .modal-content p"); // modal 내부 내용
 
+// addHidden, removeHidden - 요소의 class="hidden"을 지우고 없애는 함수
 
-//addHidden, removeHidden - 요소의 class="hidden"을 지우고 없애는 함수
 // 알림창 Open
 function openModal(cameraNumber) {
     removeHidden(modal);
     modalContent.innerText = `${cameraNumber}번 카메라 위치에서 신호가 들어왔습니다.`;
 }
+
 // 알림창 Close 및 상태 기본 값 초기화
 function closeModal() {
-    resetWebcam();
+    resetCam();
     addHidden(modal);
     addHidden(crashBtn);
     addHidden(reportBtn1);
     addHidden(reportBtn2);
-    removeHidden(cam1Text);
-    removeHidden(cam2Text);
-    cam2Text.classList.remove("change_Location"); //텍스트 위치
     focusCamValue = "0";
     crashValue = false;
 }
+
 // 해제 버튼을 눌렀을 때
 function clearAndSendWebSocketMessage() {
     const message = "clear";
     ws8767.send(message);
     closeModal();
 }
-
-clearBtn.addEventListener("click", clearAndSendWebSocketMessage);
-mainClearBtn.addEventListener("click", clearAndSendWebSocketMessage);
-
 
 // 신고 버튼을 눌렀을 때
 function handleReportClick(location) {
@@ -44,26 +39,31 @@ function handleReportClick(location) {
         closeModal();
     }
 }
-// 위치1 신고버튼
+
+// -------------------------- click시 동작 Start -----------------------
+// 해제 버튼 click
+clearBtn.addEventListener("click", clearAndSendWebSocketMessage);
+mainClearBtn.addEventListener("click", clearAndSendWebSocketMessage);
+
+// 위치1 신고 버튼 click
 reportBtn1.addEventListener("click", () => {
     handleReportClick(1);
 });
-// 위치2 신고버튼
+// 위치2 신고 버튼 click
 reportBtn2.addEventListener("click", () => {
     handleReportClick(2);
 });
-// 다른 신호가 들어왔을 때 생기는 특정 버튼을 눌렀을 때
+// 다른 신호가 들어왔을 때 생기는 crash 버튼 click
 crashBtn.addEventListener("click", () => {
-    crashValue = true;
+    crashValue = true; 
     modalContent.innerText = `확인 후 적절한 조치를 부탁드립니다.`;
     addHidden(crashBtn);
-    resetWebcam();
+    resetCam();
     removeHidden(reportBtn1);
     removeHidden(reportBtn2);
-    removeHidden(cam1Text);
-    removeHidden(cam2Text);
-    cam2Text.classList.remove("change_Location");
 });
+// -------------------------- click시 동작 End-----------------------
+
 
 // WebSocket 8767번 연결: 데이터 송신 전용 소켓
 const ws8767 = new WebSocket(`ws://${ipAddress}:8767`);
